3a35fbeb73648401a02e1a7699520ef4aa7e3fed
Peter Hillerström <peter.hillerstrom@soprano.fi>
2012-11-12 13:47:24 +0200

    Fixed the erroneous header name for Etag. Who didn't run the online tests?

--- library/ZendService/Amazon/S3/S3.php
+++ library/ZendService/Amazon/S3/S3.php
@@ -232,7 +232,7 @@ class S3 extends \ZendService\Amazon\AbstractAmazon
             $info['type']  = $headers->get('Content-type');
             $info['size']  = $headers->get('Content-length');
             $info['mtime'] = $headers->get('Last-modified');
-            $info['etag']  = $headers->get('Content-type');
+            $info['etag']  = $headers->get('Etag');
 
             //Prevents from the fatal error method call on a non-object
             foreach ($info as $key => $value)
