5de36a4bbb7409e20255026c27ededc60e0cafa4
Matthew Weier O'Phinney <matthew@zend.com>
2015-06-04 14:12:29 -0500

    Fix CS issues reported by php-cs-fixer
    - Fixes only the specific issues related to this commit

--- library/ZendService/Amazon/SimpleDb/Attribute.php
+++ library/ZendService/Amazon/SimpleDb/Attribute.php
@@ -45,7 +45,7 @@ class Attribute
      *
      * @return string
      */
-    public function getItemName ()
+    public function getItemName()
     {
         return $this->_itemName;
     }
@@ -65,7 +65,7 @@ class Attribute
      *
      * @return string
      */
-    public function getName ()
+    public function getName()
     {
         return $this->_name;
     }
