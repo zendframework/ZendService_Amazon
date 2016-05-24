c48da448d7a179a6648b37cf84f06c90cb90197a
Elvis Ciotti <elvis@Elviss-MacBook-Pro.local>
2016-05-25 00:52:22 +0200

    update Item Image attribute PHPdoc hints

--- library/ZendService/Amazon/Item.php
+++ library/ZendService/Amazon/Item.php
@@ -46,17 +46,17 @@ class Item
     public $AverageRating;
 
     /**
-     * @var string
+     * @var Image
      */
     public $SmallImage;
 
     /**
-     * @var string
+     * @var Image
      */
     public $MediumImage;
 
     /**
-     * @var string
+     * @var Image
      */
     public $LargeImage;
 
