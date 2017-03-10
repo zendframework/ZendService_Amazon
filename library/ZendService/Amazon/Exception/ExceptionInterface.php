4f1cdfa687432dce82b2e8af4cbd02b39fb3e27e
Demian Katz <demian.katz@villanova.edu>
2017-03-10 10:47:14 -0500

    Removed obsolete lines from headers.

--- library/ZendService/Amazon/Exception/ExceptionInterface.php
+++ library/ZendService/Amazon/Exception/ExceptionInterface.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
  * @see       https://github.com/zendframework/ZendService_Amazon for the canonical source repository
  * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/ZendService_Amazon/blob/master/LICENSE.md New BSD License
- * @package   Zend_Service
  */
 
 namespace ZendService\Amazon\Exception;
